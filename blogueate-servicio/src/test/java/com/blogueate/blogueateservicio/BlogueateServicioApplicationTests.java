ac84905eba106bd6b26257b125666a0ace43e8ab
Leonardo Pereyra P <i202401202@cibertec.edu.pe>
2025-08-23 10:42:47 -0500

    Subo blogearte-servicio en un mismo repo

---
+++ blogueate-servicio/src/test/java/com/blogueate/blogueateservicio/BlogueateServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogueate.blogueateservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogueateServicioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
